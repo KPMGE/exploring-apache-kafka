52199557dbea534f74f59bd7a25022eca5249d65
KPMGE <jesuskevin254@gmail.com>
2024-02-04 10:17:05 -0300

    :sparkles: create producer and send message to kafka

--- producer-consumer-rs/src/main.rs
+++ producer-consumer-rs/src/main.rs
@@ -1,3 +1,19 @@
+use rdkafka::{producer::{Producer, BaseProducer, BaseRecord}, ClientConfig};
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let producer: BaseProducer = ClientConfig::new()
+        .set("bootstrap.servers", "127.0.0.1:9094")
+        .create()
+        .expect("Could not create producer!");
+
+    let _ = producer.send(
+        BaseRecord::<String, str>::to("test")
+            .payload("Test from rust")
+    ).map_err(|e| {
+        eprintln!("ERROR: could not send the message!");
+        panic!("{:?}", e);
+    });
+
+    let _ = producer.flush(Duration::from_secs(1));
 }
